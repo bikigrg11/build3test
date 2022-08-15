54400edfaaebf5fcaa6b7c96ba30f138f29a004c
bikigrg11 <bikigurung8@gmail.com>
2022-08-15 04:06:15 -0400

    sensitive data checking

--- part2.sh
+++ part2.sh
@@ -21,5 +21,19 @@ git commit -m "$gitMessage"
 
 git log
 
+## Checking if the files contatins sensitive information
+## i.e Phone no before pushing the file to the Git Remote
+
+while IFS='' read -r line || [[ -n $line ]]
+do
+    if [[ "$line" =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$|^\([0-9]{3}\)\ [0-9]{3}-[0-9]{4}$ ]]
+    then
+        echo "$line"
+    fi
+done < *
+
+
+#git push --all
+
 exit 0
 
