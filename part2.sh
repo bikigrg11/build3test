abb838a860d7324c437942814aab2ceda08c18c6
bikigrg11 <bikigurung8@gmail.com>
2022-08-15 05:01:46 -0400

    check

--- part2.sh
+++ part2.sh
@@ -24,7 +24,7 @@ git commit -m "$gitMessage"
 
 #filePath = pwd
 
-flag = 'true'
+flag = 0
 
 echo "Checking the file"
 
@@ -33,11 +33,12 @@ do
     if [[ "$line" =~ ^[0-9]{3}-[0-9]{3}-[0-9]{4}$|^\([0-9]{3}\)\ [0-9]{3}-[0-9]{4}$|^[0-9]{9}$ ]]
     then
         echo "$line"
-	$flag = 'false'
+	$flag = 1
     fi
 done <newfile.txt
 
-if [$flag = 'true']; then
+if [$flag == 0];
+	then
 	echo "flag is clear"
 	git push --all
 else
